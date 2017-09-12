715cc44deeed8ffb47bc5257f34383a8ce31e50f
William D. Jones <thor0505@comcast.net>
2017-09-12 18:58:28 -0400

    Meson compile working successfully (not tested).

--- tools/bmp2vbch.py
+++ tools/bmp2vbch.py
@@ -1,11 +1,11 @@
+#!/usr/bin/python2
+
 import optparse
 import os.path
 import struct
 
-import bmpreader
-import palettetools as pt
-import vbgfx
-import carray
+from vb_py import bmpreader, vbgfx, carray
+from vb_py import palettetools as pt
 
 
 version = 0.9
@@ -21,37 +21,37 @@ def bmp2vbch_cmd(bmp_in, charfile = None, compress=True, bgprefix="bg_", charpre
 		outchar_filename = os.path.splitext(outchar_filename_ext)[0]
 	#print bmp_in
 	#global_palette_dict = ['\x00\x00\x00' : 0, '\x80\x00\x00' : 1, '\xC0\x00\x00' : 2, '\xFF\x00\x00' : 3}
-	
+
 	bmp2vbch_header = "\n/* <<<<<<<<<< Generated using the bmp2vbch.py tool, ver" + str(version) + " >>>>>>>>>> */\n\n"
-	
+
 	chararray_writer = carray.ArrayWriter(OffsetComments=True, Header = bmp2vbch_header)
 	bgarray_writer = carray.ArrayWriter(OffsetComments=True, OffsetInc = 8, Header = bmp2vbch_header)
 	global_palette = pt.PaletteTable('\x00\x00\x00\x80\x00\x00\xC0\x00\x00\xFF\x00\x00')
 	vb_scene = vbgfx.VBScene(global_palette)
-	
+
 	bmp_list = []
 	palimg_list = []
 	tileimg_list = []
-	
+
 	for bmpfile in bmp_in:
 		bmp_list.append(bmpreader.Bitmap(bmpfile))
-	
+
 	#Must specify at least the minimum number of colors to fill a single
 	#palette table. This data will be preserved verbatim.
 	#Other palettes can be omitted/hueristically generated from the "master palette".
-	
+
 	for bmpimg in bmp_list:
 		palimg_list.append(bmpimg.to_palette_image(global_palette))
-		
+
 	for palimg in palimg_list:
 		tileimg_list.append(pt.TileImage(palimg, 8, 8))
-		
+
 	#This section determines whether the supplied bitmaps and extracted chars
 	#meet conditions by the hardware. It also modifies the images in any
 	#manner deemend necessary.
 	for tileimg in tileimg_list:
 		vb_scene.add_image(tileimg)
-	
+
 	#tile_idx = 0
 	char_str = vb_scene.charstr_raw()
 	"""for tile in bmpreader.read_n_bytes(all_tiles, 16):
@@ -60,28 +60,28 @@ def bmp2vbch_cmd(bmp_in, charfile = None, compress=True, bgprefix="bg_", charpre
 			char_dict[tile] = struct.pack('>H', tile_idx)
 			char_str = char_str + tile
 			tile_idx = tile_idx + 1"""
-	
+
 	outchar_arrayname = charprefix + outchar_filename
-	char_outname = outchar_path + os.path.sep + outchar_filename_ext	
+	char_outname = os.path.join(outchar_path, outchar_filename_ext)
 	chararray_writer.write_file(char_outname, char_str, outchar_arrayname, 2048*8)
-	
+
 	for (bmpfile, index) in zip(bmp_in, range(0, len(vb_scene))):
 		bmp_path, bmp_filename_ext = os.path.split(bmpfile)
 		bmp_filename = os.path.splitext(bmp_filename_ext)[0]
 		bmp_arrayname = bgprefix + bmp_filename
 		#bmp_outname = bmp_path + os.path.sep + bmp_filename + ".c"
-		
+
 		#BMP C file side effects are output into the same directory as
 		#the char file.
 		bmp_outname = os.path.join(outchar_path, bmp_filename + ".c")
-		
+
 		#bgindex = ''
 		#for tile in bmpreader.read_n_bytes(bgmap, 16):
 		#	print struct.unpack('>H', char_dict[tile])
 		#	bgindex = bgindex + char_dict[tile]
-		bg_str = vb_scene.bg_raw(index)	
+		bg_str = vb_scene.bg_raw(index)
 		bgarray_writer.write_file(bmp_outname, bg_str, bmp_arrayname)
-	
+
 if __name__ == '__main__':
 	parser = optparse.OptionParser(usage="Usage: %prog [options] bmp1 [bmp2 ...]")
 	parser.add_option("-b", None, action="store",  type="string", \
@@ -96,9 +96,9 @@ if __name__ == '__main__':
 	parser.add_option("-o", None, action="store", type="string", \
 		dest="charfile", help="Output character file (default: Name of first bitmap, prefixed with 'c')", default=None)
 	(options, args) = parser.parse_args()
-	
+
 	if not args:
 		print "No input files specified. Aborting."
 		exit(-2)
-		
+
 	bmp2vbch_cmd(args, options.charfile, options.compress, options.bgprefix, options.charprefix)
